6d9061108292e944aff49e2a089c261686ebe1eb
scandas <43444227+scandas@users.noreply.github.com>
2018-09-23 00:05:50 +0200

    Update revert.sh
    using patch instead of git apply

--- patches/revert.sh
+++ patches/revert.sh
@@ -3,15 +3,15 @@
 MYABSPATH=$(readlink -f "$0")
 PATCHBASE=$(dirname "$MYABSPATH")
 CMBASE=$(readlink -f "$PATCHBASE/../../../../")
+echo $CMBASE
 
-for i in $(find "$PATCHBASE"/* -type d); do
-	PATCHNAME=$(basename "$i")
-	PATCHTARGET=$PATCHNAME
-	for i in $(seq 4); do
-		PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
-		if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
+for dir in $(find "$PATCHBASE"/* -type d); do
+	PATCHDIR=$(basename "$dir")
+	PATCHTARGET=$(echo $PATCHDIR | sed -e 's:_:/:g')
+	for patch in $(find "$dir"/* -type f); do
+		PATCHNAME=$(basename "$patch")
+		echo applying $PATCHDIR/$PATCHNAME to $PATCHTARGET
+		cd "$CMBASE/$PATCHTARGET" || exit 1
+		patch -p1 -R < "$PATCHBASE/$PATCHDIR/$PATCHNAME" #|| exit 1
 	done
-	echo applying $PATCHNAME to $PATCHTARGET
-	cd "$CMBASE/$PATCHTARGET" || exit 1
-	git apply -R --3way "$PATCHBASE/$PATCHNAME"/* || exit 1
 done
