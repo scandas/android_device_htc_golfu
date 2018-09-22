e9bcec51d4b9ea0130d5f6b795d21a330efb4307
scandas <43444227+scandas@users.noreply.github.com>
2018-09-22 23:52:15 +0200

    Update apply.sh
    using patch instead of git apply

--- patches/apply.sh
+++ patches/apply.sh
@@ -5,14 +5,13 @@ PATCHBASE=$(dirname "$MYABSPATH")
 CMBASE=$(readlink -f "$PATCHBASE/../../../../")
 echo $CMBASE
 
-for i in $(find "$PATCHBASE"/* -type d); do
-	PATCHNAME=$(basename "$i")
-	PATCHTARGET=$PATCHNAME
-	for i in $(seq 4); do
-		PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
-		if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
+for dir in $(find "$PATCHBASE"/* -type d); do
+	PATCHDIR=$(basename "$dir")
+	PATCHTARGET=$(echo $PATCHDIR | sed -e 's:_:/:g')
+	for patch in $(find "$dir"/* -type f); do
+		PATCHNAME=$(basename "$patch")
+		echo applying $PATCHDIR/$PATCHNAME to $PATCHTARGET
+		cd "$CMBASE/$PATCHTARGET" || exit 1
+		patch -p1 --dry-run --verbose < "$PATCHBASE/$PATCHDIR/$PATCHNAME" #|| exit 1
 	done
-	echo applying $PATCHNAME to $PATCHTARGET
-	cd "$CMBASE/$PATCHTARGET" || exit 1
-	git apply --3way "$PATCHBASE/$PATCHNAME"/* || exit 1
 done
